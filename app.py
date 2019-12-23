47d79a673916043c22f45e2d0356aa9ba87041b8
michelnosseir <nosseir.michel@gmail.com>
2019-12-23 16:06:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,5 @@
-import subprocess
-subprocess.run(["main"])
+import subprocess ,os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+exepath = dir_path+"\main"
+print(exepath)
+subprocess.run([exepath])
